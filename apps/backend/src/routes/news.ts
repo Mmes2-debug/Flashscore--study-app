b6d5be72c13d14f0ea39300a9422e2937725381e
MAGAJIco <97481521+MAGAJIco@users.noreply.github.com>
2025-10-21 00:24:51 +0100

    Update news.ts

--- apps/backend/src/routes/news.ts
+++ apps/backend/src/routes/news.ts
@@ -1,5 +1,5 @@
 import { FastifyInstance, FastifyRequest, FastifyReply } from "fastify";
-import News from "../models/News";
+import News from "../models/News.js";
 
 // Define body type for creating news
 interface CreateNewsBody {
